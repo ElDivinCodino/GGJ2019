5a3ed5d4e61f49889833cbec9f372460f39d0f3a
Eldivincodino <francyalz@hotmail.it>
2019-01-26 02:48:17 +0100

    Aggiustata la banana

--- BananaRace/Assets/Scripts/BananaMovement.cs
+++ BananaRace/Assets/Scripts/BananaMovement.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class BananaMovement : MonoBehaviour
 {
-    public float forwardSpeed = 1;
-    public float lateralRotation = 1;
-    public float jumpForce = 1;
+    public Transform rocketSx, rocketDx, rocketCentral;
+    public float speed, maxForce;
+
     Rigidbody rb;
 
     void Start()
@@ -16,12 +16,25 @@ public class BananaMovement : MonoBehaviour
 
     void Update()
     {
-        rb.AddRelativeTorque(transform.forward * Input.GetAxis("Vertical") * forwardSpeed * Time.deltaTime);
-        rb.AddRelativeTorque(transform.right * Input.GetAxis("Horizontal") * lateralRotation * Time.deltaTime);
+        if (Mathf.Abs(rb.velocity.magnitude) < maxForce)
+        {
+            rb.AddForceAtPosition(transform.forward * Input.GetAxis("Vertical") * speed * 200 * Time.deltaTime, rocketCentral.position);
+
+            float horizontal = Input.GetAxis("Horizontal");
+
+            if (horizontal > 0)
+            {
+                transform.RotateAround(transform.position, Vector3.up, 40 * Time.deltaTime);
+            }
+            else if (horizontal < 0)
+            {
+                transform.RotateAround(transform.position, Vector3.up, -40 * Time.deltaTime);
+            }
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity += Vector3.up * jumpForce;
+            rb.velocity += Vector3.up * speed;
         }
     }
 }
