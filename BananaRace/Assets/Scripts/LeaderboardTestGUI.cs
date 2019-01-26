6be924033bee0963f825492c1d97db4af61fc498
nick1392 <niccolo.bisagno@unitn.it>
2019-01-26 10:47:48 +0100

    leadearboard all in canvas

--- BananaRace/Assets/Scripts/LeaderboardTestGUI.cs
+++ BananaRace/Assets/Scripts/LeaderboardTestGUI.cs
@@ -13,42 +13,24 @@ public class LeaderboardTestGUI : MonoBehaviour
 {
     private string _nameInput = "";
     private string _scoreInput = "0";
+    private string text;
 
-    private void OnGUI()
+    public string fillLeaderboard()
     {
-        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
-
-        // Display high scores!
+        text = "";
         for (int i = 0; i < Leaderboard.EntryCount; ++i)
         {
             var entry = Leaderboard.GetEntry(i);
-            GUILayout.Label("Name: " + entry.name + ", Score: " + entry.score);
+            text += "Name: " + entry.name + ", Score: " + entry.score +"\n";
         }
 
-        // Interface for reporting test scores.
-        GUILayout.Space(10);
+        return text;
+    }
 
-        _nameInput = GUILayout.TextField(_nameInput);
-        _scoreInput = GUILayout.TextField(_scoreInput);
-
-        //if (GUILayout.Button("Record"))
-        //{
-        //    int score;
-        //    int.TryParse(_scoreInput, out score);
-
-        //    Leaderboard.Record(_nameInput, score);
-
-        //    // Reset for next input.
-        //    _nameInput = "";
-        //    _scoreInput = "0";
-        //}
-
-        //if (GUILayout.Button("Clear"))
-        //{
-        //    Leaderboard.Clear();
-        //}
-
-        GUILayout.EndArea();
+    public void insertName(string playerName)
+    {
+        _nameInput = playerName;
+        _scoreInput = 1000.ToString(); //TODO: insert score here dynamically
     }
 
     public void clearLeaderboard()
@@ -66,5 +48,6 @@ public class LeaderboardTestGUI : MonoBehaviour
         // Reset for next input.
         _nameInput = "";
         _scoreInput = "0";
+
     }
 }
---
+++ BananaRace/Assets/updateLeaderboard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateLeaderboard : MonoBehaviour
+{
+    public GameObject leaderboard;
+    private string lead;
+
+    private void OnEnable()
+    {
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        lead = leaderboard.GetComponent<LeaderboardTestGUI>().fillLeaderboard();
+        Debug.Log(lead);
+        this.GetComponent<Text>().text = lead;
+    }
+
+}
