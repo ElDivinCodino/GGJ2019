d5b584d5d330e6a2c53278ce960c3fb946240811
DarkTiger <darktigerdevelop@gmail.com>
2019-01-26 15:29:40 +0100

    Created GameManager prefab and AudioManager script

---
+++ BananaRace/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip musicInGame;
+    public AudioClip startRaceBeep;
+    public AudioSource audioSourceMusic;
+    public AudioSource audioSourceSFX;
+
+
+    public void PlayMusicInGame()
+    {
+        if (musicInGame)
+        {
+            audioSourceMusic.clip = musicInGame;
+            audioSourceMusic.Play();
+        }
+    }
+}
