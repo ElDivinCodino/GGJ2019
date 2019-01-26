22e201327d65b0d5a38a0be65be82afd66b8d1f4
nick1392 <niccolo.bisagno@unitn.it>
2019-01-26 19:04:09 +0100

    menu linked to one VS one, countdown

---
+++ BananaRace/Assets/Scripts/countdown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class countdown : MonoBehaviour
+{
+    public GameObject text;
+
+    void Start()
+    {
+        StartCoroutine(Countdown(3));
+    }
+
+    IEnumerator Countdown(int seconds)
+    {
+        int count = seconds;
+
+        while (count > 0)
+        {
+            text.GetComponent<AudioSource>().Play();
+            text.GetComponent<TextMeshProUGUI>().text = count.ToString();
+            yield return new WaitForSeconds(1);
+            count--;
+        }
+
+        // count down is finished...
+        StartCoroutine(StartGame(1));
+    }
+
+    IEnumerator StartGame(int seconds)
+    {
+        // do something...
+        text.GetComponent<AudioSource>().Play();
+        text.GetComponent<TextMeshProUGUI>().text = "GO!";
+        yield return new WaitForSeconds(1);
+        text.SetActive(false);
+    }
+}
